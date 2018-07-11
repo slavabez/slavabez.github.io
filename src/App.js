acb7ef297b259053027cd154d5ff7c16e734d8b0
Slava Bezgachev <bezgachev@gmail.com>
2018-07-11 12:59:55 +0600

    Fixed link and metadata

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
 import AppWrapper from "./styled/AppWrapper";
-import ScrollablePane from "./styled/ScrolalblePane";
 import FrontPage from "./styled/FrontPage";
 import FloatingContent from "./styled/FloatingContent";
 import { Header, Bio, LogoLink, LogoWrapper } from "./styled/textCollection";
@@ -31,46 +30,41 @@ class App extends React.Component {
     return (
       <ThemeProvider theme={this.state.theme}>
         <AppWrapper>
-          <ScrollablePane>
-            <FrontPage>
-              <ThemeSwitchButton handleThemeToggle={this.handleThemeToggle} />
-              <FloatingContent>
-                <Header>Slava Bezgachev</Header>
-                <Bio>
-                  I am a full stack web developer, lately focusing on mastering
-                  Javascript. I like working with NodeJS and ReactJS, check out
-                  my GitHub page for some work I've done and check out my{" "}
-                  <a href="">portfolio</a> where I take some in-depth looks
-                  into some of my projects.
-                </Bio>
+          <FrontPage>
+            <ThemeSwitchButton handleThemeToggle={this.handleThemeToggle} />
+            <FloatingContent>
+              <Header>Slava Bezgachev</Header>
+              <Bio>
+                I am a full stack web developer, lately focusing on mastering
+                Javascript. I like working with NodeJS and ReactJS, check out my
+                GitHub page for some work I've done and check out my{" "}
+                <a href="https://bezgachev.com">portfolio</a> where I take some in-depth looks into
+                some of my projects.
+              </Bio>
 
-                <Bio>
-                  Best way to contact me is by email{" "}
-                  <a href="mailto:slava@bezgachev.com">slava@bezgachev.com</a>
-                </Bio>
-                <LogoWrapper>
-                  <LogoLink target="_blank" href="https://github.com/slavabez">
-                    <GithubIcon />
-                  </LogoLink>
-                  <LogoLink
-                    target="_blank"
-                    href="https://medium.com/@bezgachev"
-                  >
-                    <MediumIcon />
-                  </LogoLink>
-                  <LogoLink
-                    target="_blank"
-                    href="https://stackoverflow.com/users/story/2237238"
-                  >
-                    <StackOverflowIcon />
-                  </LogoLink>
-                  <LogoLink target="_blank" href="https://twitter.com/slavabez">
-                    <TwitterIcon />
-                  </LogoLink>
-                </LogoWrapper>
-              </FloatingContent>
-            </FrontPage>
-          </ScrollablePane>
+              <Bio>
+                Best way to contact me is by email{" "}
+                <a href="mailto:slava@bezgachev.com">slava@bezgachev.com</a>
+              </Bio>
+              <LogoWrapper>
+                <LogoLink target="_blank" href="https://github.com/slavabez">
+                  <GithubIcon />
+                </LogoLink>
+                <LogoLink target="_blank" href="https://medium.com/@bezgachev">
+                  <MediumIcon />
+                </LogoLink>
+                <LogoLink
+                  target="_blank"
+                  href="https://stackoverflow.com/users/story/2237238"
+                >
+                  <StackOverflowIcon />
+                </LogoLink>
+                <LogoLink target="_blank" href="https://twitter.com/slavabez">
+                  <TwitterIcon />
+                </LogoLink>
+              </LogoWrapper>
+            </FloatingContent>
+          </FrontPage>
         </AppWrapper>
       </ThemeProvider>
     );
